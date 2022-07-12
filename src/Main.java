d1f0a53a92cdbc64ab577929aae2db50096f21e6
Homework1 <danisholha760@gmail.com>
2022-07-11 19:40:09 -0500

    Exercise 1 completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    // Exercise 1
+        byte avocado = 2;
+        int tomato = 6;
+        short cucumber = 10;
+        long potato = 18L;
+        float porridgWeight = 15.5f;
+        double buckwheatWeight = 3.5;
+        double differenceCereal = porridgWeight - buckwheatWeight;
+        System.out.println("Total weight of vegetables " + avocado + tomato + cucumber + potato + " kg");
+        System.out.println("Difference between cereals " + differenceCereal + " kg");
     }
 }
